11bef50c156a785d5804d54dfcf5e61934fcb0c3
Louis Garman <louisgarman@gmail.com>
2021-06-19 21:01:56 +0100

    Fix comment

--- cmd/ots/system_directories.go
+++ cmd/ots/system_directories.go
@@ -2,13 +2,13 @@ package main
 
 import "os"
 
-// Abstraction around methods that retrieve system directories
+// Abstraction around methods that retrieve system directories - expressly for
+// the purpose of substituting with fakes
 type Directories interface {
 	UserHomeDir() (string, error)
 }
 
-// Wrapper around os.* funcs - expressly for the purpose substituting with fakes
-// for testing purposes
+// Wrapper around os.* funcs
 type SystemDirectories struct{}
 
 var _ Directories = (*SystemDirectories)(nil)
