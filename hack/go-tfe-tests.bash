52c2ce831efae6d14c50e00aa355c381600bbf48
Louis Garman <louisgarman@gmail.com>
2021-07-01 06:58:59 +0100

    Drop unnec use of jq

--- hack/go-tfe-tests.bash
+++ hack/go-tfe-tests.bash
@@ -5,5 +5,5 @@ set -ex
 export TFE_TOKEN=dummy
 export TFE_ADDRESS=https://localhost:8080
 
-cd $(go list -m -u -json github.com/leg100/go-tfe | jq -r '.Dir')
+cd $(go list -f '{{.Dir}}' github.com/leg100/go-tfe)
 go test -v -run TestWorkspacesList
