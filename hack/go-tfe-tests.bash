98d272d228f6aed2c957791f87219c0b9d86e372
Louis Garman <louisgarman@gmail.com>
2021-09-26 20:59:22 +0100

    Bump go-tfe

--- hack/go-tfe-tests.bash
+++ hack/go-tfe-tests.bash
@@ -9,7 +9,8 @@ export TFE_TOKEN=dummy
 export TFE_ADDRESS=https://localhost:8080
 export SKIP_PAID=1
 
+# Default subset of tests
 TESTS="${@:-Test(Workspaces(Create|List|Update|Delete|Unlock|Lock)|Organizations(Create|List|Read|Update)|StateVersions|Runs|Plans|Applies(Read|Logs)|ConfigurationVersions)}"
 
-cd $(go mod download -json github.com/hashicorp/go-tfe@v0.17.0 | jq -r '.Dir')
+cd $(go mod download -json github.com/hashicorp/go-tfe@v0.17.1 | jq -r '.Dir')
 go test -v -run $TESTS
