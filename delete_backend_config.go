e984015d02b9ae03e4490caad0a3ca2d4aa36d48
Louis Garman <louisgarman@gmail.com>
2022-03-11 21:21:38 +0000

    wip

--- delete_backend_config.go
+++ delete_backend_config.go
@@ -45,25 +45,21 @@ func deleteBackendConfigFromDirectory(ctx context.Context, dir string) error {
 }
 
 func deleteBackendConfig(config []byte) (bool, []byte, error) {
-	var deleted bool
-
 	f, diags := hclwrite.ParseConfig([]byte(config), "", hcl.Pos{Line: 1, Column: 1})
 	if diags.HasErrors() {
 		return false, nil, fmt.Errorf("unable to parse HCL: %s", diags.Error())
 	}
 
-OuterLoop:
 	for _, block := range f.Body().Blocks() {
 		if block.Type() == "terraform" {
 			for _, b2 := range block.Body().Blocks() {
 				if b2.Type() == "backend" {
 					block.Body().RemoveBlock(b2)
-					deleted = true
-					break OuterLoop
+					return true, f.Bytes(), nil
 				}
 			}
 		}
 	}
 
-	return deleted, f.Bytes(), nil
+	return false, f.Bytes(), nil
 }
