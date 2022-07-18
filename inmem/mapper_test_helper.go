2cf9e3b59aa24ce7953420a697a38ec252679dad
Louis Garman <louisgarman@gmail.com>
2022-07-18 21:22:16 +0100

    wip

---
+++ inmem/mapper_test_helper.go
@@ -0,0 +1,42 @@
+package inmem
+
+import (
+	"context"
+
+	"github.com/leg100/otf"
+)
+
+type fakeWorkspaceService struct {
+	workspaces []*otf.Workspace
+
+	otf.WorkspaceService
+}
+
+func (f *fakeWorkspaceService) List(ctx context.Context, opts otf.WorkspaceListOptions) (*otf.WorkspaceList, error) {
+	return &otf.WorkspaceList{
+		Items:      f.workspaces,
+		Pagination: otf.NewPagination(opts.ListOptions, len(f.workspaces)),
+	}, nil
+}
+
+type fakeRunService struct {
+	runs []*otf.Run
+
+	otf.RunService
+}
+
+func (f *fakeRunService) List(ctx context.Context, opts otf.RunListOptions) (*otf.RunList, error) {
+	return &otf.RunList{
+		Items:      f.runs,
+		Pagination: otf.NewPagination(opts.ListOptions, len(f.runs)),
+	}, nil
+}
+
+type fakeSubject struct {
+	// name of organization the subject is a member of
+	memberOrg string
+}
+
+func (f *fakeSubject) CanAccess(org *string) bool {
+	return *org == f.memberOrg
+}
