6541c7d00f1ac5a42f66952a8d8cf01a49c3fd1c
Louis Garman <louisgarman@gmail.com>
2022-07-18 21:25:41 +0100

    wip

--- inmem/mapper.go
+++ inmem/mapper.go
@@ -6,7 +6,18 @@ import (
 	"github.com/leg100/otf"
 )
 
-// Mapper is an in-memory mapping of resource identifiers
+// Mapper is an in-memory implementation of a mapper.
+//
+// A mapper maintains mappings between various resource identifiers, which are
+// used by upstream layers to make decisions and efficiently lookup resources.
+//
+// For instance, the authorization layer needs to decide whether to permit
+// access and cannot do so based on a single identifier (e.g. a run id) but
+// needs to know which organization and workspace id it relates to.
+//
+// Whereas the persistence layer, with access to mappings, need only lookup
+// resources based on the most appropriate identifier for which it maintains an
+// index, rather having to support lookups using a multitude of identifiers.
 type Mapper struct {
 	workspaces *workspaceMapper
 	runs       *runMapper
