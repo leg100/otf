da8a580c0b59bcb71de2ea9e0e540e04c22fadde
Louis Garman <louisgarman@gmail.com>
2022-08-28 17:08:02 +0100

    wip

--- http/html/run_test.go
+++ http/html/run_test.go
@@ -2,30 +2,31 @@ package html
 
 import (
 	"context"
+	"crypto/tls"
+	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
-	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/gorilla/mux"
 	"github.com/leg100/otf"
 	"github.com/r3labs/sse/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_TailLogs(t *testing.T) {
+func TestTailLogs(t *testing.T) {
 	run := otf.NewTestRun(t, otf.TestRunCreateOptions{})
-
-	// setup channel - send a chunk and then close
-	chunks := make(chan []byte, 1)
-	chunks <- []byte("some logs")
-	close(chunks)
 
 	// setup SSE server
 	srv := sse.New()
 	srv.AutoStream = true
 	srv.AutoReplay = false
+
+	// setup logs channel - send a chunk and then close
+	chunks := make(chan []byte, 1)
+	chunks <- []byte("some logs")
+	close(chunks)
 
 	// fake app
 	app := &Application{
@@ -37,32 +38,27 @@ func Test_TailLogs(t *testing.T) {
 		Logger: logr.Discard(),
 	}
 
-	// setup req and resp
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	mux.SetURLVars(r, map[string]string{"run_id": run.ID()})
-	r.URL.RawQuery = url.Values{
-		"offset": []string{"0"},
-		"stream": []string{"tail-123"},
-		"phase":  []string{"plan"},
-	}.Encode()
+	// setup web server
+	router := mux.NewRouter()
+	router.HandleFunc("/{run_id}", app.tailRun)
+	webSrv := httptest.NewTLSServer(router)
+	defer webSrv.Close()
 
-	// run handler in bg and wait for it to respond
-	go app.tailRun(w, r)
+	// setup SSE client and subscribe to stream
+	client := sse.NewClient(webSrv.URL + "/" + run.ID() + "?offset=0&stream=tail-123&phase=plan")
+	client.Connection.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	events := make(chan *sse.Event, 99)
+	require.NoError(t, client.SubscribeChan("tail-123", events))
+	defer client.Unsubscribe(events)
 
-	time.Sleep(time.Second)
-
-	assert.Equal(t, 200, w.Code)
-	want := `id: 
-data: {"offset":9,"html":"some logs\u003cbr\u003e"}
-event: new-log-chunk
-
-id: 
-data: no more logs
-event: finished
-
-`
-	assert.Equal(t, want, w.Body.String())
+	logs := <-events
+	assert.Equal(t, "new-log-chunk", string(logs.Event))
+	assert.Equal(t, "{\"offset\":9,\"html\":\"some logs\\u003cbr\\u003e\"}", string(logs.Data))
+	finished := <-events
+	assert.Equal(t, "finished", string(finished.Event))
+	assert.Equal(t, "no more logs", string(finished.Data))
 }
 
 type fakeTailApp struct {
