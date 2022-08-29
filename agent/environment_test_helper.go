2d5cff94771f97b5be76bbd13e81ee8d8f407dc0
Louis Garman <louisgarman@gmail.com>
2022-08-29 21:05:29 +0100

    wip

--- agent/environment_test_helper.go
+++ agent/environment_test_helper.go
@@ -15,17 +15,6 @@ func (j *fakeJob) Do(e otf.Environment) error {
 	return e.RunCLI(j.cmd, j.args...)
 }
 
-// discard is a no-op io.WriteCloser
-type discard struct{}
-
-func (discard) Write(p []byte) (int, error) {
-	return len(p), nil
-}
-
-func (discard) Close() error {
-	return nil
-}
-
 type fakeWriteCloser struct {
 	*bytes.Buffer
 }
