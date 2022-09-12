1217299a20cd814e90d189124cad9c02ea597397
Louis Garman <louisgarman@gmail.com>
2022-09-12 09:14:35 +0100

    wip

--- sql/pub_sub.go
+++ sql/pub_sub.go
@@ -75,6 +75,7 @@ func (ps *PubSub) Start(ctx context.Context) error {
 	for {
 		notification, err := conn.Conn().WaitForNotification(ctx)
 		if err != nil {
+			ps.Error(err, "waiting for postgres notification")
 			return err
 		}
 
