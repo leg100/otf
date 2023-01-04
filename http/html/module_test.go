c22592f7f1fa1f82c6f030b445f663e2ba6f69c9
Louis Garman <louisgarman@gmail.com>
2023-01-04 18:32:36 +0000

    wip

--- http/html/module_test.go
+++ http/html/module_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/leg100/otf"
+	"github.com/leg100/otf/http/html/paths"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -84,7 +85,64 @@ func TestNewModule_Repo(t *testing.T) {
 	}
 }
 
+func TestNewModule_Confirm(t *testing.T) {
+	org := otf.NewTestOrganization(t)
+	provider := otf.NewTestVCSProvider(t, org)
+	app := newFakeWebApp(t, &fakeModulesApp{
+		provider: provider,
+	})
+
+	q := "/?organization_name=acme-corp&vcs_provider_id=vcs-123&identifier=leg100/terraform-otf-test"
+	r := httptest.NewRequest("GET", q, nil)
+	w := httptest.NewRecorder()
+	app.newModuleConfirm(w, r)
+	if !assert.Equal(t, 200, w.Code) {
+		t.Log(w.Body.String())
+	}
+}
+
+func TestNewModule_Create(t *testing.T) {
+	org := otf.NewTestOrganization(t)
+	provider := otf.NewTestVCSProvider(t, org)
+	mod := otf.NewTestModule(org)
+	app := newFakeWebApp(t, &fakeModulesApp{
+		org:      org,
+		provider: provider,
+		mod:      mod,
+	})
+
+	q := "/?organization_name=acme-corp&vcs_provider_id=vcs-123&identifier=leg100/terraform-otf-test"
+	r := httptest.NewRequest("GET", q, nil)
+	w := httptest.NewRecorder()
+	app.createModule(w, r)
+	if assert.Equal(t, 302, w.Code) {
+		redirect, err := w.Result().Location()
+		require.NoError(t, err)
+		assert.Equal(t, paths.Module(mod.ID()), redirect.Path)
+	}
+}
+
+func TestNewModule_Delete(t *testing.T) {
+	org := otf.NewTestOrganization(t)
+	mod := otf.NewTestModule(org)
+	app := newFakeWebApp(t, &fakeModulesApp{
+		org: org,
+		mod: mod,
+	})
+
+	q := "/?module_id=mod-123"
+	r := httptest.NewRequest("GET", q, nil)
+	w := httptest.NewRecorder()
+	app.deleteModule(w, r)
+	if assert.Equal(t, 302, w.Code) {
+		redirect, err := w.Result().Location()
+		require.NoError(t, err)
+		assert.Equal(t, paths.Modules(org.Name()), redirect.Path)
+	}
+}
+
 type fakeModulesApp struct {
+	org      *otf.Organization
 	mod      *otf.Module
 	provider *otf.VCSProvider
 	tarball  []byte
@@ -93,7 +151,19 @@ type fakeModulesApp struct {
 	otf.Application
 }
 
+func (f *fakeModulesApp) PublishModule(context.Context, otf.PublishModuleOptions) (*otf.Module, error) {
+	return f.mod, nil
+}
+
+func (f *fakeModulesApp) GetOrganization(context.Context, string) (*otf.Organization, error) {
+	return f.org, nil
+}
+
 func (f *fakeModulesApp) GetModuleByID(context.Context, string) (*otf.Module, error) {
+	return f.mod, nil
+}
+
+func (f *fakeModulesApp) DeleteModule(context.Context, string) (*otf.Module, error) {
 	return f.mod, nil
 }
 
@@ -103,6 +173,10 @@ func (f *fakeModulesApp) ListModules(context.Context, otf.ListModulesOptions) ([
 
 func (f *fakeModulesApp) DownloadModuleVersion(context.Context, otf.DownloadModuleOptions) ([]byte, error) {
 	return f.tarball, nil
+}
+
+func (f *fakeModulesApp) GetVCSProvider(context.Context, string) (*otf.VCSProvider, error) {
+	return f.provider, nil
 }
 
 func (f *fakeModulesApp) ListVCSProviders(context.Context, string) ([]*otf.VCSProvider, error) {
