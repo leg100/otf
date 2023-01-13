abd15e5c67273aff21b2cf797775f5553a58330e
Louis Garman <louisgarman@gmail.com>
2023-01-13 19:21:54 +0000

    wip

--- http/variable_client.go
+++ http/variable_client.go
@@ -9,7 +9,7 @@ import (
 )
 
 func (c *client) ListVariables(ctx context.Context, workspaceID string) ([]*otf.Variable, error) {
-	u := fmt.Sprintf("workspaces/%s/variables", workspaceID)
+	u := fmt.Sprintf("workspaces/%s/vars", workspaceID)
 	req, err := c.newRequest("GET", u, nil)
 	if err != nil {
 		return nil, err
