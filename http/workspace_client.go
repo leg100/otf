97d826712f745b5f87b665c542589b40f36d1703
Louis Garman <louisgarman@gmail.com>
2023-01-16 10:58:49 +0000

    wip

--- http/workspace_client.go
+++ http/workspace_client.go
@@ -90,7 +90,7 @@ func (c *client) ListWorkspaces(ctx context.Context, options otf.WorkspaceListOp
 }
 
 // UpdateWorkspace updates the settings of an existing workspace.
-func (c *client) UpdateWorkspaceByID(ctx context.Context, workspaceID string, options otf.WorkspaceUpdateOptions) (*otf.Workspace, error) {
+func (c *client) UpdateWorkspace(ctx context.Context, workspaceID string, options otf.WorkspaceUpdateOptions) (*otf.Workspace, error) {
 	if err := options.Valid(); err != nil {
 		return nil, err
 	}
