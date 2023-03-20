e202023afe9f0f24ed1b4d4caa8d3ab881c80549
Louis Garman <louisgarman@gmail.com>
2023-03-20 21:39:45 +0000

    wip

--- chunk_test.go
+++ chunk_test.go
@@ -1,68 +1,67 @@
-package logs
+package otf
 
 import (
 	"testing"
 
-	"github.com/leg100/otf"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestChunkCut(t *testing.T) {
 	tests := []struct {
 		name  string
-		chunk otf.Chunk
-		opts  otf.GetChunkOptions
-		want  otf.Chunk
+		chunk Chunk
+		opts  GetChunkOptions
+		want  Chunk
 	}{
 		{
 			name: "cut nothing",
-			chunk: otf.Chunk{
+			chunk: Chunk{
 				Data: []byte("\x02hello world\x03"),
 			},
-			want: otf.Chunk{
+			want: Chunk{
 				Data: []byte("\x02hello world\x03"),
 			},
 		},
 		{
 			name: "cut middle",
-			chunk: otf.Chunk{
+			chunk: Chunk{
 				Data: []byte("\x02hello world\x03"),
 			},
-			opts: otf.GetChunkOptions{Offset: 3, Limit: 4},
-			want: otf.Chunk{
+			opts: GetChunkOptions{Offset: 3, Limit: 4},
+			want: Chunk{
 				Data:   []byte("llo "),
 				Offset: 3,
 			},
 		},
 		{
 			name: "sanitize excessive limit",
-			chunk: otf.Chunk{
+			chunk: Chunk{
 				Data: []byte("\x02hello world\x03"),
 			},
-			opts: otf.GetChunkOptions{Offset: 3, Limit: 99},
-			want: otf.Chunk{
+			opts: GetChunkOptions{Offset: 3, Limit: 99},
+			want: Chunk{
 				Data:   []byte("llo world\x03"),
 				Offset: 3,
 			},
 		},
 		{
 			name: "handle excessive offset",
-			chunk: otf.Chunk{
+			chunk: Chunk{
 				Data: []byte("\x02hello world\x03"),
 			},
-			opts: otf.GetChunkOptions{Offset: 99},
-			want: otf.Chunk{
+			opts: GetChunkOptions{Offset: 99},
+			want: Chunk{
 				Offset: 13,
 			},
 		},
 		{
 			name: "cut chunk with non-zero offset",
-			chunk: otf.Chunk{
+			chunk: Chunk{
 				Data:   []byte(" world\x03"),
 				Offset: 6,
 			},
-			opts: otf.GetChunkOptions{Offset: 8, Limit: 12},
-			want: otf.Chunk{
+			opts: GetChunkOptions{Offset: 8, Limit: 12},
+			want: Chunk{
 				Data:   []byte("orld\x03"),
 				Offset: 8,
 			},
