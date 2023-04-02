91b0059bf597b0ab44ed2c34369c8a0a22a95296
Louis Garman <louisgarman@gmail.com>
2023-04-02 20:29:26 +0100

    wip

--- integration/sync_test.go
+++ integration/sync_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/leg100/otf"
 	"github.com/leg100/otf/auth"
 	"github.com/leg100/otf/cloud"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,6 +18,39 @@ func TestSync(t *testing.T) {
 	ctx := otf.AddSubjectToContext(context.Background(), &auth.SiteAdmin)
 
 	svc := setup(t, nil)
-	err := svc.Sync(ctx, cloud.User{Name: "bobby"})
+	err := svc.Sync(ctx, cloud.User{
+		Name: "bobby",
+		Teams: []cloud.Team{
+			{Name: "owners", Organization: "acme-corp"},
+		},
+	})
 	require.NoError(t, err)
+
+	// should have created an organization named acme-corp
+	_, err = svc.GetOrganization(ctx, "acme-corp")
+	assert.NoError(t, err)
+
+	// and made them an owner of acme-corp
+	isOwner(t, svc, "bobby", "bobby")
+
+	// should have created a personal organization
+	_, err = svc.GetOrganization(ctx, "bobby")
+	assert.NoError(t, err)
+
+	// and made them an owner of their personal organization
+	isOwner(t, svc, "bobby", "bobby")
+}
+
+func isOwner(t *testing.T, svc *testDaemon, organization, username string) {
+	// perform all actions as superuser
+	ctx := otf.AddSubjectToContext(context.Background(), &auth.SiteAdmin)
+
+	// and made them an owner of acme-corp
+	owners, err := svc.GetTeam(ctx, organization, "owners")
+	assert.NoError(t, err)
+	members, err := svc.ListTeamMembers(ctx, owners.ID)
+	require.NoError(t, err)
+	if assert.Equal(t, 1, len(members)) {
+		assert.Equal(t, members[0].Username, username)
+	}
 }
