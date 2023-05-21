e94448eacb9a5dc20a418540acff4256a1287ff3
Louis Garman <louisgarman@gmail.com>
2023-05-21 16:43:28 +0100

    wip

--- internal/http/html/static/js/main.js
+++ internal/http/html/static/js/main.js
@@ -1,16 +1,17 @@
 window.addEventListener('load', (e) => {
-    // enables copy to clipboard functionality for identifiers
-    const identifiers = document.querySelectorAll('.clipboard-copyable');
+    // enables copy to clipboard functionality
+    const identifiers = document.querySelectorAll('.clipboard-icon');
     identifiers.forEach(function(id) {
         id.addEventListener('click', function(event) {
-            content = event.target.innerHTML;
+            content = event.target.previousElementSibling.innerHTML;
             navigator.clipboard.writeText(content);
+
+            // show notification momentarily but only if there isn't already a notification showing
             notification = event.target.parentNode.querySelector('.copied-notification');
             if (notification === null) {
                 let span = document.createElement('span');
                 span.className = 'copied-notification';
                 span.innerHTML = 'copied!';
-                // show notification momentarily
                 event.target.parentNode.appendChild(span);
                 setTimeout(function() {
                     event.target.parentNode.removeChild(span);
