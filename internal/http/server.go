9e3a6630d3eec946c32a0067d5ac3bcb02e0b21a
Guimo <guimo.spritekin@gmail.com>
2023-06-21 14:07:36 +1000

    Add trailing slash to make path more compatible with TFC
    Problem described in https://github.com/leg100/otf/issues/474

--- internal/http/server.go
+++ internal/http/server.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	ModuleV1Prefix = "/v1/modules"
-	APIPrefixV2    = "/api/v2"
+	APIPrefixV2    = "/api/v2/"
 
 	// shutdownTimeout is the time given for outstanding requests to finish
 	// before shutdown.
