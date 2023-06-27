6bbaf549c4567e406b306449b171e33dac280683
Louis Garman <louisgarman@gmail.com>
2023-06-27 10:26:00 +0100

    wip

---
+++ internal/integration/db_test.go
@@ -0,0 +1,33 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/leg100/otf/internal/logr"
+	"github.com/leg100/otf/internal/sql"
+	"github.com/stretchr/testify/require"
+)
+
+// TestWaitAndLock tests acquiring a connection from a pool, obtaining a session
+// lock and then releasing lock and the connection, and it does this several
+// times, to demonstrate that it is returning resources and not running into
+// limits.
+func TestWaitAndLock(t *testing.T) {
+	integrationTest(t)
+
+	ctx := context.Background()
+	db, err := sql.New(ctx, sql.Options{
+		Logger:     logr.Discard(),
+		ConnString: sql.NewTestDB(t),
+	})
+	require.NoError(t, err)
+	t.Cleanup(db.Close)
+
+	for i := 0; i < 100; i++ {
+		func() {
+			err := db.WaitAndLock(ctx, 123, func() error { return nil })
+			require.NoError(t, err)
+		}()
+	}
+}
--- internal/integration/repo_db_test.go
+++ internal/integration/repo_db_test.go
@@ -8,6 +8,8 @@ import (
 
 // TestRepoDB tests repo package's interaction with a (real) database.
 func TestRepoDB(t *testing.T) {
+	integrationTest(t)
+
 	// Call out to integration test situated within 'repo' package, which then
 	// calls private methods
 	repo.TestDB(t)
