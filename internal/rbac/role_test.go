4cb02f8b99e44bd2386f2471b905924596fa5d89
Justin <justin.li@optable.co>
2023-11-17 13:50:05 -0500

    fix: Add missing CancelRunAction to WorkspaceWriteRole
    Members with WorspaceAdminRole cannot manually cancel pending runs in a
    workspace. This PR adds the missing permission to the role.

--- internal/rbac/role_test.go
+++ internal/rbac/role_test.go
@@ -12,12 +12,14 @@ func TestRole_IsAllowed(t *testing.T) {
 	assert.True(t, WorkspacePlanRole.IsAllowed(TailLogsAction))
 
 	assert.True(t, WorkspaceWriteRole.IsAllowed(ApplyRunAction))
+	assert.True(t, WorkspaceWriteRole.IsAllowed(CancelRunAction))
 	assert.True(t, WorkspaceWriteRole.IsAllowed(CreateRunAction))
 	assert.True(t, WorkspaceWriteRole.IsAllowed(ListRunsAction))
 	assert.True(t, WorkspaceWriteRole.IsAllowed(TailLogsAction))
 
 	assert.True(t, WorkspaceAdminRole.IsAllowed(SetWorkspacePermissionAction))
 	assert.True(t, WorkspaceAdminRole.IsAllowed(ApplyRunAction))
+	assert.True(t, WorkspaceWriteRole.IsAllowed(CancelRunAction))
 	assert.True(t, WorkspaceAdminRole.IsAllowed(CreateRunAction))
 	assert.True(t, WorkspaceAdminRole.IsAllowed(ListRunsAction))
 	assert.True(t, WorkspaceAdminRole.IsAllowed(TailLogsAction))
@@ -25,6 +27,7 @@ func TestRole_IsAllowed(t *testing.T) {
 	assert.True(t, WorkspaceManagerRole.IsAllowed(CreateWorkspaceAction))
 	assert.True(t, WorkspaceManagerRole.IsAllowed(SetWorkspacePermissionAction))
 	assert.True(t, WorkspaceManagerRole.IsAllowed(ApplyRunAction))
+	assert.True(t, WorkspaceWriteRole.IsAllowed(CancelRunAction))
 	assert.True(t, WorkspaceManagerRole.IsAllowed(CreateRunAction))
 	assert.True(t, WorkspaceManagerRole.IsAllowed(ListRunsAction))
 	assert.True(t, WorkspaceManagerRole.IsAllowed(TailLogsAction))
