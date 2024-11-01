9523d0d2e08e22cb1a345180de2d268153afedfc
Louis Garman <louisgarman@gmail.com>
2024-11-01 20:04:37 +0000

    wip

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -70,6 +70,9 @@ func newRunner(
 	isAgent bool,
 	cfg Config,
 ) (*Runner, error) {
+	if cfg.MaxJobs == 0 {
+		cfg.MaxJobs = DefaultMaxJobs
+	}
 	r := &Runner{
 		RunnerMeta: &RunnerMeta{
 			Name:    cfg.Name,
@@ -88,9 +91,6 @@ func newRunner(
 		r.v = 1
 		// Distinguish log messages on server runner from other components.
 		r.logger = logger.WithValues("component", "runner")
-	}
-	if cfg.MaxJobs == 0 {
-		cfg.MaxJobs = DefaultMaxJobs
 	}
 	if cfg.Debug {
 		r.logger.V(r.v).Info("enabled debug mode")
