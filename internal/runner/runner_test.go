41f5669ef362dc42507b8a8c61c074e022c9bb02
Louis Garman <75728+leg100@users.noreply.github.com>
2024-11-12 08:37:01 +0000

    fix: runner unable to re-register (#707)

---
+++ internal/runner/runner_test.go
@@ -0,0 +1,67 @@
+package runner
+
+import (
+	"context"
+	"testing"
+
+	"github.com/leg100/otf/internal/logr"
+	"github.com/leg100/otf/internal/resource"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRunner(t *testing.T) {
+	updates := make(chan RunnerStatus)
+	wantID := resource.NewID(resource.RunnerKind)
+
+	r, err := newRunner(
+		logr.Discard(),
+		&fakeRunnerClient{registeredID: wantID, updates: updates},
+		&fakeOperationSpawner{},
+		false,
+		Config{},
+	)
+	require.NoError(t, err)
+
+	// Terminate runner at end of test
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	startErr := make(chan error)
+	go func() {
+		startErr <- r.Start(ctx)
+	}()
+
+	// Test that runner registers itself
+	assert.Equal(t, &RunnerMeta{ID: wantID}, <-r.registered)
+	// Terminate runner
+	cancel()
+	// Test that runner sends final status update
+	assert.Equal(t, RunnerExited, <-updates)
+}
+
+type fakeRunnerClient struct {
+	client
+
+	registeredID resource.ID
+	updates      chan RunnerStatus
+}
+
+func (f *fakeRunnerClient) register(ctx context.Context, opts registerOptions) (*RunnerMeta, error) {
+	return &RunnerMeta{ID: f.registeredID}, nil
+}
+
+func (f *fakeRunnerClient) getJobs(ctx context.Context, agentID resource.ID) ([]*Job, error) {
+	// Block until context canceled
+	<-ctx.Done()
+	return nil, nil
+}
+
+func (f *fakeRunnerClient) updateStatus(ctx context.Context, agentID resource.ID, status RunnerStatus) error {
+	f.updates <- status
+	return nil
+}
+
+type fakeOperationSpawner struct {
+	operationSpawner
+}
---
+++ internal/runner/unregistered.go
@@ -0,0 +1,14 @@
+package runner
+
+import "github.com/leg100/otf/internal/authz"
+
+// unregistered describes a runner that is not yet registered.
+type unregistered struct {
+	// unregistered is a subject only for the purposes of satisfying the
+	// token-handling middleware which doesn't call any of the interface
+	// methods.
+	authz.Subject
+
+	// pool is non-nil if the runner is an agent.
+	pool *RunnerMetaAgentPool
+}
