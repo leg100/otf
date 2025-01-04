bebaa7355ff622a591ada5a092c384f437e7d5b4
Louis Garman <louisgarman@gmail.com>
2025-01-04 10:37:13 +0000

    wip

--- internal/http/html/static/js/main.js
+++ internal/http/html/static/js/main.js
@@ -1,35 +1,35 @@
 // https://daverupert.com/2017/11/happier-html5-forms/
-window.addEventListener('load', (e) => {
+window.addEventListener("load", () => {
   const inputs = document.querySelectorAll("input, select, textarea");
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     input.addEventListener(
       "invalid",
-      event => {
+      (event) => {
         input.classList.add("error");
       },
-      false
+      false,
     );
-    input.addEventListener("blur", function() {
+    input.addEventListener("blur", function () {
       input.checkValidity();
     });
   });
 });
 
 // https://css-tricks.com/block-links-the-search-for-a-perfect-solution/#method-4-sprinkle-javascript-on-the-second-method
-document.addEventListener('alpine:init', () => {
-  Alpine.data('block_link', (block, link) => ({
+document.addEventListener("alpine:init", () => {
+  Alpine.data("block_link", (block, link) => ({
     init() {
       block.classList.add("cursor-pointer", "hover:bg-gray-100");
-      block.addEventListener("click", (e) => {
+      block.addEventListener("click", () => {
         isTextSelected = window.getSelection().toString();
         if (!isTextSelected) {
           location = link;
         }
       });
-      links = block.querySelectorAll("a");
-      links.forEach(link => {
+      const links = block.querySelectorAll("a");
+      links.forEach((link) => {
         link.addEventListener("click", (e) => e.stopPropagation());
       });
-    }
-  }))
-})
+    },
+  }));
+});
