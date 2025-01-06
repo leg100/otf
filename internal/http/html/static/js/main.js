5928197e644ce17cb2d7890e14ad7e998c6e860e
Louis Garman <louisgarman@gmail.com>
2025-01-06 17:32:02 +0000

    wip

--- internal/http/html/static/js/main.js
+++ internal/http/html/static/js/main.js
@@ -1,20 +1,3 @@
-// https://daverupert.com/2017/11/happier-html5-forms/
-window.addEventListener("load", () => {
-  const inputs = document.querySelectorAll("input, select, textarea");
-  inputs.forEach((input) => {
-    input.addEventListener(
-      "invalid",
-      (event) => {
-        input.classList.add("error");
-      },
-      false,
-    );
-    input.addEventListener("blur", function () {
-      input.checkValidity();
-    });
-  });
-});
-
 window.addEventListener("load", () => {
   const anchors = document.querySelectorAll("#sidemenu a");
   anchors.forEach((element) => {
