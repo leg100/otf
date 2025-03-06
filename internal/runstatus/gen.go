4a9010cef78d5e891ce45b4195e4d710fc524208
Louis Garman <louisgarman@gmail.com>
2025-03-06 18:09:27 +0000

    wip

--- internal/runstatus/gen.go
+++ internal/runstatus/gen.go
@@ -1,10 +1,33 @@
-package runstatus
+//go:build ignore
+
+package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/leg100/otf/internal/runstatus"
 )
+
+//go:generate go run gen.go
+
+var themeMappings = map[runstatus.Status]string{
+	runstatus.Applied:            "success",
+	runstatus.ApplyQueued:        "secondary",
+	runstatus.Applying:           "accent",
+	runstatus.Canceled:           "warning",
+	runstatus.Confirmed:          "info",
+	runstatus.Discarded:          "warning",
+	runstatus.Errored:            "error",
+	runstatus.ForceCanceled:      "warning",
+	runstatus.Pending:            "primary",
+	runstatus.PlanQueued:         "secondary",
+	runstatus.Planned:            "info",
+	runstatus.PlannedAndFinished: "success",
+	runstatus.Planning:           "primary",
+}
 
 var components = []string{
 	"badge",
@@ -14,15 +37,17 @@ var components = []string{
 }
 
 func main() {
-	f, err := os.Create("class_names.txt")
-	if err != nil {
-		log.Fatal("Error: ", err.Error())
-	}
-	defer f.Close()
+	var b strings.Builder
 
-	for status, semantic := range ThemeMappings {
+	for status, semantic := range themeMappings {
 		for _, component := range components {
-			f.WriteString(fmt.Sprintf("%s-%s", status.String(), semantic))
+			b.WriteString(fmt.Sprintf(".%s-%s {\n", component, status.String()))
+			b.WriteString(fmt.Sprintf("\t@apply %s-%s;\n", component, semantic))
+			b.WriteString("}\n")
 		}
+	}
+
+	if err := os.WriteFile("../http/html/static/css/runstatus.css", []byte(b.String()), 0o755); err != nil {
+		log.Fatal("Error: ", err.Error())
 	}
 }
