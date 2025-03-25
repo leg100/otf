5bd3f756f3e5c5815244542962830dfce94fe45c
Louis Garman <louisgarman@gmail.com>
2025-03-25 10:51:07 +0000

    wip

--- internal/logs/db.go
+++ internal/logs/db.go
@@ -10,46 +10,73 @@ import (
 	"github.com/leg100/otf/internal/sql"
 )
 
-var q = &Queries{}
-
 // pgdb is a logs database on postgres
 type pgdb struct {
 	*sql.DB // provides access to generated SQL queries
 }
 
 func (db *pgdb) put(ctx context.Context, chunk Chunk) error {
-	err := q.InsertLogChunk(ctx, db.Conn(ctx), InsertLogChunkParams{
-		ChunkID: chunk.TfeID,
-		RunID:   chunk.RunID,
-		Phase:   sql.String(string(chunk.Phase)),
-		Chunk:   chunk.Data,
-		Offset:  sql.Int4(chunk.Offset),
-	})
-	if err != nil {
-		return sql.Error(err)
-	}
-	return nil
+	_, err := db.Exec(ctx, `
+INSERT INTO logs (
+    chunk_id,
+    run_id,
+    phase,
+    chunk,
+    _offset
+) VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)`,
+		chunk.ID,
+		chunk.RunID,
+		chunk.Phase,
+		chunk.Data,
+		chunk.Offset,
+	)
+	return err
 }
 
 func (db *pgdb) getChunk(ctx context.Context, chunkID resource.TfeID) (Chunk, error) {
-	chunk, err := q.FindLogChunkByID(ctx, db.Conn(ctx), chunkID)
-	if err != nil {
-		return Chunk{}, sql.Error(err)
-	}
-	return Chunk{
-		TfeID:  chunkID,
-		RunID:  chunk.RunID,
-		Phase:  internal.PhaseType(chunk.Phase.String),
-		Data:   chunk.Chunk,
-		Offset: int(chunk.Offset.Int32),
-	}, nil
+	rows := db.Query(ctx, `
+SELECT
+    chunk_id,
+    run_id,
+    phase,
+    chunk,
+    _offset AS offset
+FROM logs
+WHERE chunk_id = $1
+`, chunkID)
+	return sql.CollectOneRow(rows, func(row pgx.CollectableRow) (Chunk, error) {
+		var chunk Chunk
+		err := row.Scan(
+			&chunk.ID,
+			&chunk.RunID,
+			&chunk.Phase,
+			&chunk.Data,
+			&chunk.Offset,
+		)
+		return chunk, err
+	})
 }
 
 func (db *pgdb) getLogs(ctx context.Context, runID resource.TfeID, phase internal.PhaseType) ([]byte, error) {
-	data, err := q.FindLogs(ctx, db.Conn(ctx), FindLogsParams{
-		RunID: runID,
-		Phase: sql.String(string(phase)),
-	})
+	rows := db.Query(ctx, `
+SELECT
+    string_agg(chunk, '')
+FROM (
+    SELECT run_id, phase, chunk
+    FROM logs
+    WHERE run_id = $1
+    AND   phase  = $2
+    ORDER BY _offset
+) c
+GROUP BY run_id, phase
+`, runID, phase)
+	logs, err := sql.CollectOneRow(rows, pgx.RowTo[[]byte])
 	if err != nil {
 		// Don't consider no rows an error because logs may not have been
 		// uploaded yet.
@@ -58,5 +85,5 @@ func (db *pgdb) getLogs(ctx context.Context, runID resource.TfeID, phase interna
 		}
 		return nil, sql.Error(err)
 	}
-	return data, nil
+	return logs, nil
 }
