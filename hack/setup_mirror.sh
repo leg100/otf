6a5dafa37e8ed3170f660d3a4820c7031390e9be
Louis Garman <louisgarman@gmail.com>
2025-04-23 22:54:26 +0100

    wip

--- hack/setup_mirror.sh
+++ hack/setup_mirror.sh
@@ -1,13 +1,5 @@
+# Setup filesystem mirror for both terraform and tofu. This speeds up the
+# integration tests massively.
 cd mirror/
 terraform providers mirror .
 tofu providers mirror .
-
-cat > mirror.tfrc <<-EOF
-provider_installation {
-  filesystem_mirror {
-    path = "${PWD}"
-  }
-}
-EOF
-
-echo "now set TF_CLI_CONFIG_FILE=${PWD}/mirror.tfrc"
