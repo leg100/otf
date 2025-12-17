a44d37af67305139e85128beb9b3d4002969f334
Mark Glines <mark@glines.org>
2025-12-17 15:43:41 -0500

    Add an integration test for params -target and -replace

---
+++ internal/integration/runner_params_test.go
@@ -0,0 +1,90 @@
+package integration
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/leg100/otf/internal"
+	runpkg "github.com/leg100/otf/internal/run"
+	"github.com/leg100/otf/internal/runstatus"
+	"github.com/leg100/otf/internal/vcs"
+	"github.com/leg100/otf/internal/workspace"
+	"github.com/stretchr/testify/require"
+)
+
+// TestIntegration_RunnerParams tests passing of plan parameters.
+func TestIntegration_RunnerParams(t *testing.T) {
+	integrationTest(t)
+
+	// setup daemon along with fake github repo
+	repo := vcs.NewRandomRepo()
+	svc, org, ctx := setup(t)
+	t.Logf("repo == %s", repo.Name())
+
+	ws, err := svc.Workspaces.Create(ctx, workspace.CreateOptions{
+		Name:         internal.Ptr("test"),
+		Organization: &org.Name,
+	})
+	require.NoError(t, err)
+
+	config := []byte(fmt.Sprintf(`terraform {
+  cloud {
+    hostname = "%s"
+    organization = "%s"
+
+    workspaces {
+      name = "%s"
+    }
+  }
+}
+resource "random_uuid" "id1" {}
+resource "random_uuid" "id2" {}
+resource "random_uuid" "id3" {}
+`, svc.System.Hostname(), org.Name, ws.Name))
+
+	true_ := true
+
+	applyThing := func(t *testing.T, opt *runpkg.CreateOptions) *runpkg.Report {
+		// upload config
+		cv := svc.createConfigurationVersion(t, ctx, ws, nil)
+		path := t.TempDir()
+		err = os.WriteFile(filepath.Join(path, "main.tf"), []byte(config), 0o777)
+		require.NoError(t, err)
+		tarball, err := internal.Pack(path)
+		require.NoError(t, err)
+		err = svc.Configs.UploadConfig(ctx, cv.ID, tarball)
+		require.NoError(t, err)
+
+		// create run
+		opt.AutoApply = &true_
+		run := svc.createRun(t, ctx, ws, cv, opt)
+
+		// let it run to completion
+		rv := svc.waitRunStatus(t, ctx, run.ID, runstatus.Applied)
+		return rv.Plan.ResourceReport
+	}
+
+	t.Run("provision the first resource", func(t *testing.T) {
+		plan := applyThing(t, &runpkg.CreateOptions{
+			TargetAddrs: []string{"random_uuid.id1"},
+		})
+		require.Equal(t, &runpkg.Report{Additions: 1}, plan)
+	})
+	t.Run("provision the second resource, replace the first one", func(t *testing.T) {
+		plan := applyThing(t, &runpkg.CreateOptions{
+			TargetAddrs:  []string{"random_uuid.id1", "random_uuid.id2"},
+			ReplaceAddrs: []string{"random_uuid.id1"},
+		})
+		require.Equal(t, &runpkg.Report{Additions: 2, Destructions: 1}, plan)
+	})
+
+	t.Run("delete the first resource", func(t *testing.T) {
+		plan := applyThing(t, &runpkg.CreateOptions{
+			TargetAddrs: []string{"random_uuid.id1"},
+			IsDestroy:   &true_,
+		})
+		require.Equal(t, &runpkg.Report{Destructions: 1}, plan)
+	})
+}
