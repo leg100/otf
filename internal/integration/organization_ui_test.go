6ca1cf1faea5e17bb0975a4e428aceec5a0e10c8
Louis Garman <louisgarman@gmail.com>
2026-04-05 18:15:56 +0100

    wip

--- internal/integration/organization_ui_test.go
+++ internal/integration/organization_ui_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/leg100/otf/internal/ui/paths"
@@ -14,63 +15,92 @@ func TestIntegration_OrganizationUI(t *testing.T) {
 
 	daemon, _, ctx := setup(t, skipDefaultOrganization())
 
-	// test creating/updating/deleting
-	browser.New(t, ctx, func(page playwright.Page) {
+	t.Run("create", func(t *testing.T) {
+		browser.New(t, ctx, func(page playwright.Page) {
+			// go to the list of organizations
+			_, err := page.Goto(daemon.URL(paths.Organizations()))
+			require.NoError(t, err)
 
-		// go to the list of organizations
-		_, err := page.Goto(daemon.URL(paths.Organizations()))
-		require.NoError(t, err)
+			// add an org
+			err = page.Locator("#new-organization-button").Click()
+			require.NoError(t, err)
 
-		// add an org
-		err = page.Locator("#new-organization-button").Click()
-		require.NoError(t, err)
+			err = page.Locator("input#name").Fill("acme-corp")
+			require.NoError(t, err)
+			screenshot(t, page, "new_org_enter_name")
 
-		err = page.Locator("input#name").Fill("acme-corp")
-		require.NoError(t, err)
-		screenshot(t, page, "new_org_enter_name")
+			err = page.Locator("input#name").Press("Enter")
+			require.NoError(t, err)
 
-		err = page.Locator("input#name").Press("Enter")
-		require.NoError(t, err)
+			err = expect.Locator(page.GetByRole("alert")).ToHaveText(
+				"created organization: acme-corp",
+			)
+			require.NoError(t, err)
 
-		err = expect.Locator(page.GetByRole("alert")).ToHaveText(
-			"created organization: acme-corp",
-		)
-		require.NoError(t, err)
-		screenshot(t, page, "new_org_created")
+			screenshot(t, page, "new_org_created")
+		})
+	})
 
-		// go to the list of organizations
-		_, err = page.Goto(daemon.URL(paths.Organizations()))
-		require.NoError(t, err)
+	t.Run("list", func(t *testing.T) {
+		org1 := daemon.createOrganization(t, ctx)
+		org2 := daemon.createOrganization(t, ctx)
+		org3 := daemon.createOrganization(t, ctx)
 
-		// there should be one organization listed
-		err = expect.Locator(page.Locator(`//table/tbody/tr`)).ToHaveCount(1)
-		require.NoError(t, err)
+		browser.New(t, ctx, func(page playwright.Page) {
+			// go to the list of organizations
+			_, err := page.Goto(daemon.URL(paths.Organizations()))
+			require.NoError(t, err)
 
-		// go to organization
-		err = page.Locator(`//tr[@id='org-item-acme-corp']/td[1]/a`).Click()
-		require.NoError(t, err)
+			// check three orgs are listed
+			err = expect.Locator(page.Locator("tr#org-item-" + org1.Name.String())).ToBeVisible()
+			require.NoError(t, err)
+			err = expect.Locator(page.Locator("tr#org-item-" + org2.Name.String())).ToBeVisible()
+			require.NoError(t, err)
+			err = expect.Locator(page.Locator("tr#org-item-" + org3.Name.String())).ToBeVisible()
+			require.NoError(t, err)
 
-		// go to organization settings
-		err = page.Locator("#menu-item-settings > a").Click()
-		require.NoError(t, err)
+		})
+	})
 
-		// change organization name
-		err = page.Locator("input#name").Clear()
-		require.NoError(t, err)
-		err = page.Locator("input#name").Fill("super-duper-org")
-		require.NoError(t, err)
+	t.Run("settings", func(t *testing.T) {
+		org1 := daemon.createOrganization(t, ctx)
 
-		err = page.Locator(`//button[text()='Update organization name']`).Click()
-		require.NoError(t, err)
+		browser.New(t, ctx, func(page playwright.Page) {
+			// go to the list of organizations
+			_, err := page.Goto(daemon.URL(paths.Organizations()))
+			require.NoError(t, err)
 
-		err = expect.Locator(page.GetByRole("alert")).ToHaveText("updated organization")
-		require.NoError(t, err)
+			// go to organization
+			selector := fmt.Sprintf("//tr[@id='org-item-%s']/td[1]/a", org1.Name)
+			err = page.Locator(selector).Click()
+			require.NoError(t, err)
 
-		// delete the organization
-		err = page.Locator(`//button[@id='delete-organization-button']`).Click()
-		require.NoError(t, err)
+			// go to organization's settings
+			err = page.Locator("#menu-item-settings > a").Click()
+			require.NoError(t, err)
 
-		err = expect.Locator(page.GetByRole("alert")).ToHaveText("deleted organization: super-duper-org")
-		require.NoError(t, err)
+			// change organization name
+			err = page.Locator("input#name").Clear()
+			require.NoError(t, err)
+			err = page.Locator("input#name").Fill("super-duper-org")
+			require.NoError(t, err)
+
+			err = page.Locator(`//button[text()='Update organization name']`).Click()
+			require.NoError(t, err)
+
+			err = expect.Locator(page.GetByRole("alert")).ToHaveText("updated organization")
+			require.NoError(t, err)
+
+			// go to advanced settings
+			err = page.Locator("#menu-item-advanced > a").Click()
+			require.NoError(t, err)
+
+			// delete the organization
+			err = page.Locator(`//button[@id='delete-organization-button']`).Click()
+			require.NoError(t, err)
+
+			err = expect.Locator(page.GetByRole("alert")).ToHaveText("deleted organization: super-duper-org")
+			require.NoError(t, err)
+		})
 	})
 }
