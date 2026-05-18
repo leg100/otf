b04732751d00e264cb6c0d9abcdaaa5a35fcf83b
Louis Garman <louisgarman@gmail.com>
2026-05-18 13:19:15 +0100

    wip

--- internal/workspace/mode/mode.go
+++ internal/workspace/mode/mode.go
@@ -1,9 +1,13 @@
+// Package mode handles the various execution modes for workspace runs.
 package mode
 
 type Mode string
 
 const (
+	// Remote mode executes the run via the otf server, otfd.
 	Remote Mode = "remote"
-	Local  Mode = "local"
-	Agent  Mode = "agent"
+	// Local mode executes the run on the user's local machine
+	Local Mode = "local"
+	// Agent mode executes the run via the agent, otf-agent.
+	Agent Mode = "agent"
 )
