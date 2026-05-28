2ea43490155c58e9e13c078d7c2feea7c39d2f25
Louis Garman <louisgarman@gmail.com>
2026-05-28 09:10:46 +0100

    add test

--- hack/helm-lint.sh
+++ hack/helm-lint.sh
@@ -1,5 +1,6 @@
 helm lint ./charts/otfd
 helm lint ./charts/otf-agent
+helm lint ./charts/otf-agent --values ./charts/otf-agent/tests/volume_mounts.yaml
 helm template ./charts/otfd > /dev/null
 helm template ./charts/otfd --values ./charts/otfd/tests/volume_mounts.yaml > /dev/null
 helm template ./charts/otf-agent --set token=my_agent_token --set url=https://otf.ninja > /dev/null
